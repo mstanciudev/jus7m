9269d75e7e976c3b0444cc41c70607742630e4cf
Marius Stanciu <mstanciu@192-168-0-157.rdsnet.ro>
2018-10-22 20:15:50 +0300

    First commit

---
+++ src/main/java/com/store/realsecure/RealsecureApplication.java
@@ -0,0 +1,12 @@
+package com.store.realsecure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealsecureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealsecureApplication.class, args);
+	}
+}
---
+++ src/test/java/com/store/realsecure/RealsecureApplicationTests.java
@@ -0,0 +1,16 @@
+package com.store.realsecure;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RealsecureApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
